d7dd62f5c4658887e16adb5b3da7af1b79673b47
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-06-01 21:30:53 -0300

    fix: fix final bundle size in code view component

--- src/components/BaseCodeView.vue
+++ src/components/BaseCodeView.vue
@@ -19,7 +19,14 @@
 </template>
 
 <script>
-import hljs from "highlight.js";
+import hljs from "highlight.js/lib/core";
+import css from "highlight.js/lib/languages/css";
+import xml from "highlight.js/lib/languages/xml";
+import javascript from "highlight.js/lib/languages/javascript";
+
+hljs.registerLanguage("css", css);
+hljs.registerLanguage("xml", xml);
+hljs.registerLanguage("javascript", javascript);
 
 export default {
   name: "BaseCodeView",
